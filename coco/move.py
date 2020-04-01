d702ef46bd8386fec88175cebea3d903f08719b3
Ruisi Su <rosalinesu@gmail.com>
2020-04-01 14:45:48 -0400

    added caps and names mscoco

---
+++ coco/move.py
@@ -0,0 +1,37 @@
+import shutil
+import argparse
+
+# # move
+# def move(file_name, orig_dir, dest_dir):
+#     # file_name = 'COCO_val2014_000000458401'
+#     # full_name = '{}.jpg'.format(file_name)
+#     # orig_dir = '/Users/sisi/Downloads/{}'.format(orig_dir)
+#     # dest_dir = '/Users/sisi/Downloads/{}'.format(dest_dir)
+
+
+# file_names is a txt file, each line is the file name
+def filter_move(splitType, orig_dir, dest_dir):
+    file_names = 'data2014/{}_name.txt'.format(splitType)
+    with open(file_names, 'r') as name:
+        for file_name in name:
+            file_name = file_name.rstrip()
+            file_name = file_name.split('.')[0]
+            pred_name = file_name + '.predictions'
+            # print(file_name)
+            # print(orig_dir)
+            # print(dest_dir)
+            shutil.move('{}/{}'.format(orig_dir, file_name), dest_dir)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--split', help='type of split, one of test, val, train')
+    parser.add_argument('--orig', help='original directory')
+    parser.add_argument('--to', help='destination directory')
+    args = parser.parse_args()
+
+
+    # print('split is ' + args.split)
+    # print('from ' + args.orig)
+    # print('to ' + args.to)
+    filter_move(args.split, args.orig, args.to)
---
+++ coco/readme.txt
@@ -0,0 +1,5 @@
+place the coco folder into data/ folder
+download the raw images from here: http://mscoco.org/
+and place them all into coco/train2014 and coco/val2014 . 
+You only have to do this if you wish to visualize the predictions
+
