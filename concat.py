65b7bb0291a5678521aaa1162f000cba4b8eb995
Ruisi Su <rosalinesu@gmail.com>
2020-04-08 15:49:34 -0400

    toy set

--- concat.py
+++ concat.py
@@ -1,6 +1,8 @@
 datapath = 'data/coco/mscoco/unfiltered/'
-filenames = [datapath + 'train_gold_clean.txt', datapath + 'val_gold_clean.txt', datapath + 'test_gold_clean.txt']
-with open(datapath + 'train-val-test_gold.txt', 'w') as outfile:
+# filenames = [datapath + 'train_gold_clean.txt', datapath + 'val_gold_clean.txt', datapath + 'test_gold_clean.txt']
+filenames = [datapath + 'val_gold.txt', datapath + 'test_gold.txt']
+
+with open(datapath + 'val-test_gold.txt', 'w') as outfile:
     for fname in filenames:
         with open(fname) as infile:
             for line in infile:
