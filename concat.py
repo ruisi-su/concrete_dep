7f05f945790489a7968243d37575f4faa076da92
Ruisi Su <rosalinesu@gmail.com>
2020-04-08 20:24:28 -0400

    update

--- concat.py
+++ concat.py
@@ -1,8 +1,8 @@
 datapath = 'data/coco/mscoco/unfiltered/'
-# filenames = [datapath + 'train_gold_clean.txt', datapath + 'val_gold_clean.txt', datapath + 'test_gold_clean.txt']
-filenames = [datapath + 'val_gold.txt', datapath + 'test_gold.txt']
+filenames = [datapath + 'train_gold.txt', datapath + 'val_gold.txt', datapath + 'test_gold.txt']
+# filenames = [datapath + 'val_gold.txt', datapath + 'test_gold.txt']
 
-with open(datapath + 'val-test_gold.txt', 'w') as outfile:
+with open(datapath + 'train-val-test_gold.txt', 'w') as outfile:
     for fname in filenames:
         with open(fname) as infile:
             for line in infile:
