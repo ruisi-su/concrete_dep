49a77af5fc3e6aa0e29498bacca3f865e215181a
Ruisi Su <rosalinesu@gmail.com>
2020-04-16 18:51:18 -0400

    concat conllx

--- concat.py
+++ concat.py
@@ -1,8 +1,8 @@
-datapath = 'data/coco/mscoco/unfiltered/'
+datapath = 'data/coco/mscoco/dep/'
 # filenames = [datapath + 'train_gold.txt', datapath + 'val_gold.txt', datapath + 'test_gold.txt']
-filenames = [datapath + 'val_cap_clean.txt', datapath + 'test_cap_clean.txt']
+filenames = [datapath + 'train.conllx', datapath + 'val.conllx', datapath + 'test.conllx']
 
-with open(datapath + 'val-test_cap.txt', 'w') as outfile:
+with open(datapath + 'train-val-test_dep.conllx', 'w') as outfile:
     for fname in filenames:
         with open(fname) as infile:
             for line in infile:
