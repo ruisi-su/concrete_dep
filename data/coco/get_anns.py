599c83c1eae0cfd6b6f00f6d275194de65e370fc
Ruisi Su <rosalinesu@gmail.com>
2020-04-30 14:47:39 -0400

    remove dataset

--- data/coco/get_anns.py
+++ data/coco/get_anns.py
@@ -1,45 +1,61 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from pycocotools.coco import COCO
+#from pycocotools.coco import COCO
 import numpy as np
-from nltk.tokenize import ToktokTokenizer
+# from nltk.tokenize import ToktokTokenizer
+# toktok = ToktokTokenizer()
+
 import json
 import spacy
-nlp = spacy.load("en_core_web_sm")
+# nlp = spacy.load("en_core_web_sm")
+from spacy.tokenizer import Tokenizer
+from spacy.lang.en import English
+nlp = English()
+# Create a blank Tokenizer with just the English vocab
+tokenizer = Tokenizer(nlp.vocab)
 import random
-toktok = ToktokTokenizer()
 import re
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--start', type = int,default=0,help='start idx')
+parser.add_argument('--end', type = int, default=413915,help='end idx')
+args = parser.parse_args()
 
 dataType=['train2014','val2014']
 # annFile='{}/annotations/instances_{}.json'.format(dataDir,dataType)
 annFile_train='annotations/instances_{}.json'.format(dataType[0])
 annFile_val='annotations/instances_{}.json'.format(dataType[1])
 splitFolder = './VGNSL_split/'
-splitFile = 'dataset.json'
+splitFile = 'dataset_coco.json'
 dataDir = '.'
 
-# initialize COCO api for caption annotations
-# capFile_train = 'annotations/captions_{}.json'.format(dataType[0])
-# capFile_val = 'annotations/captions_{}.json'.format(dataType[1])
-# coco_caps_train = COCO(capFile_train)
-# coco_caps_val = COCO(capFile_val)
 
-def get_image_id(splitType, im_file, log_file):
+with open(splitFile) as datafile:
+        data = json.load(datafile)
+        IMAGES = data['images']
+
+def get_image_id(splitType, start_idx=0, end_idx=5000):
     captions = []
+    im_file = splitType + '_ids_' + str(start_idx) + '.txt'
+    log_file = splitType + '_log_' + str(start_idx) + '.txt'
     # get the caps from text
     with open(splitFolder + splitType + '_caps.txt', 'r') as cap_file:
-        for c in cap_file:
+        for i, c in enumerate(cap_file):
+            if i < start_idx or i >= end_idx:
+                continue
 #             c = c.replace("**LL** ", '(').replace(" **RR**",')').lower()
 #             c = c.strip('\n').replace('.', '').replace(',',' ').replace('-', ' ').replace('/', ' ').replace(":", "").lower()
 #             c = c.replace("``", "").replace(";", "")
 #             # manual replacement
-#             c = c.replace(" 's", "s").replace(" 're", "re").replace(" n't", "nt").replace(" 'd", "d")
-#             c = c.replace('can not', 'cannot')
+            c = c.replace(" 's", "s").replace(" 're", "re").replace(" n't", "nt").replace(" 'd", "d")
+            c = c.replace('can not', 'cannot')
 #             c = c.replace("# ", '#')
 #             c = c.replace("$ ", "$")
-#             c = c.replace('1950s', '1950 s')
-#             c = c.replace(" 'd", "'d")
+            c = c.replace('1950s', '1950 s')
+            c = c.replace(" 'd", "'d")
             # manual train
             c = re.sub('[^A-Za-z0-9]+', ' ', c.lower())
             captions.append(c)
@@ -53,72 +69,38 @@ def get_image_id(splitType, im_file, log_file):
             sents = im['sentences']
             tokens = [sent['tokens'] for sent in sents]
             raws = [sent['raw'] for sent in sents]
-#             coco_set.append((im['cocoid'], tokens))
-            coco_set.append((im['cocoid'], raws))
+            coco_set.append((im['cocoid'], tokens))
+            # coco_set.append((im['cocoid'], raws))
     count = 0
     count_miss = 0
-    im_ids = []
-
-
+    #im_ids = []
     for idx, c in enumerate(captions):
-#         c_tok = toktok.tokenize(c)
-        c_tok = c.split()
+        c_tok = tokenizer(c)
+        c_tok = [token.text for token in c_tok]
+        # print(len(c_tok))
+        # c_tok = c.split()
         found = False
-
         for (cocoid, items) in coco_set:
             for item in items:
-                item = re.sub('[^A-Za-z0-9]+', ' ', item.lower())
-                item = item.split()
+                # item = re.sub('[^A-Za-z0-9]+', ' ', item.lower())
+                # item = item.split()
                 diff_1 = list(set(item).difference(set(c_tok)))
                 diff_2 = list(set(c_tok).difference(set(item)))
                 diff = max(len(diff_1), len(diff_2))
-                if diff <= 1:
-                    im_ids.append(cocoid)
+                if diff == 0:
+                    #im_ids.append(cocoid)
                     count += 1
                     found = True
                     break
             if found:
+                with open(im_file, 'a') as imfile:
+                    imfile.write(str(cocoid) + '\n')
                 break
         if not found:
             count_miss += 1
             with open(log_file, 'a') as logfile:
                 logfile.write(str(idx) + '\t' + str(c_tok) + '\t' + c + '\n')
                 print('could not found : ' + str(idx) + str(c_tok) + ' ' + c)
-
-    with open(im_file, 'w') as imfile:
-        for im_id in im_ids:
-            imfile.write(str(im_id) + '\n')
-
-get_image_id('train', 'train_ids.txt', 'train_log.txt')
-
-# sample_cap = "A woman takes a `` selfie `` with her cellphone with a cat on her shoulder .\n".lower()
-# s2 =  'A grafitti \'d mail box by a pole with a stop sign on it .'.lower()
-# print(re.sub('[^A-Za-z0-9]+', ' ', sample_cap))
-# print(sample_cap)
-# print(re.sub('[^A-Za-z0-9]+', ' ', s2.replace(" '",'')))
-
-# get COCOids by split
-# 82783 train, 5000 val, 5000 test
-# with open(splitFile) as jsf:
-#     data = json.load(jsf)
-# IMAGES = data['images']
-#
-# # return the ids for
-# dataIds = {}
-# dataIds['train'] = []
-# dataIds['test'] = []
-# dataIds['val'] = []
-# for im in IMAGES:
-#     cocoid = im['cocoid']
-#     if im['split'] == 'train':
-#         dataIds['train'].append(cocoid)
-#     elif im['split'] == 'test':
-#         dataIds['test'].append(cocoid)
-#     elif im['split'] == 'val':
-#         dataIds['val'].append(cocoid)
-#
-# size = [len(d) for d in dataIds.values()]
-# assert(sum(size) == 92783)
 
 def write_data(splitType, coco_im, coco_cap, dataIds):
     capfile = dataDir + '/' + splitType + '_cap.txt'
@@ -142,8 +124,6 @@ def write_data(splitType, coco_im, coco_cap, dataIds):
                 count += 1
     print(count)
 
-# write_data('val', coco_val, coco_caps_val, dataIds)
-
 def get_anns(anns):
     for ann in anns:
         cap = ann['caption']
@@ -161,3 +141,7 @@ def has_action(ann):
         elif token.dep_ == 'ROOT':
             return 'VERB' in [child.pos_ for child in token.children]
     return False
+
+if __name__ == '__main__':
+    # get_image_id('train', args.start, args.end)
+    get_image_id('test')
--- remove_blank.py
+++ remove_blank.py
@@ -1,14 +1,14 @@
-datapath = 'data/coco/mscoco/unfiltered/original/'
-cap_file = 'test_name.txt'
-frame_file = 'test_frame.txt'
-out_file = 'test_name_clean.txt'
-with open(datapath + cap_file, 'r') as cap, open(datapath + frame_file, 'r') as fr, open(datapath + out_file, 'w') as out:
-    for tree, frame in zip(cap, fr):
-        frame = frame.strip().split('\t')
-        if frame[0] == '':
-            continue
-        else:
-            out.write(tree)
+# datapath = 'data/coco/mscoco/unfiltered/original/'
+# cap_file = 'test_name.txt'
+# frame_file = 'test_frame.txt'
+# out_file = 'test_name_clean.txt'
+# with open(datapath + cap_file, 'r') as cap, open(datapath + frame_file, 'r') as fr, open(datapath + out_file, 'w') as out:
+#     for tree, frame in zip(cap, fr):
+#         frame = frame.strip().split('\t')
+#         if frame[0] == '':
+#             continue
+#         else:
+#             out.write(tree)
 
 
 # with open(datapath + frame_file, 'r') as fr, open(datapath + out_file, 'w') as out:
