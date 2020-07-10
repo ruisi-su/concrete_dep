832623dbdb8ea85efbf5c932d96847cc3727fa8d
Rosaline <rosalinesu@gmail.com>
2020-07-10 12:38:30 -0400

    update scripts

---
+++ compound-pcfg/test_4_step2_temp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python train.py --t_states 25 --nt_states 20 --num_epochs 20 --train_file data/multimodal_dep_3_temp/cocotrain.pkl --val_file data/multimodal_dep_3_temp/cocoval.pkl --test_file data/multimodal_dep_3_temp/cocotest.pkl --load_model dump_model/baseline_h512_nt20_t25_tembinit_b4_step2.pt --pretrained_word_emb data/vico_500.pkl --max_length 20 --final_max_length 35 --state_dim 500 --h_dim 512 --evaluate_dep --seperate_nt_emb_for_emission --head_first --tie_word_emb --seed 1022 --t_emb_init glovevico-kmeans-25-centroids-500.txt --multimodal 1 --mode test --gpu 3
