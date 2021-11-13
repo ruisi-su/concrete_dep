72d1fd32dfb70971612a1f5ad0b6de7827f32f1f
Rosaline <rosalinesu@gmail.com>
2021-11-13 14:28:55 -0500

    remove scripts

--- lpcfg/scripts/preprocess_ptb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-outputdir=$1
-
-mkdir -p $outputdir
-python preprocess.py --replace_num 1 --dep --outputfile $outputdir/ptb --inputdir ../data/ptb/ --vocabfile ../data/ptb/ptb.dict --concrete_file ../data/concreteness_modified.txt
